b7c4e557bcf242b0e4f3802b709409b24014f6b7
Light Web <118928041+lightweb-bd@users.noreply.github.com>
2026-01-09 00:43:19 +0600

    Update LaravelMailerServiceProvider.php

--- src/LaravelMailerServiceProvider.php
+++ src/LaravelMailerServiceProvider.php
@@ -7,29 +7,33 @@ use LWSoftBD\LaravelMailer\Helpers\MailConfigHelper;
 
 class LaravelMailerServiceProvider extends ServiceProvider
 {
+    public function register()
+    {
+        // ✅ config আগে merge হবে
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/laravel-mailer.php',
+            'laravel-mailer'
+        );
+    }
 
     public function boot()
     {
+        // routes only if enabled
         if (config('laravel-mailer.enabled')) {
             $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         }
 
         $this->loadViewsFrom(__DIR__.'/resources/views', 'laravel-mailer');
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-    
 
+        // config publish
         $this->publishes([
             __DIR__.'/../config/laravel-mailer.php' => config_path('laravel-mailer.php'),
         ], 'laravel-mailer-config');
 
-        MailConfigHelper::load();
-    }
-
-    public function register()
-    {
-        $this->mergeConfigFrom(
-            __DIR__.'/../config/laravel-mailer.php',
-            'laravel-mailer'
-        );
+        // ✅ Runtime-safe load (after app booted)
+        app()->booted(function () {
+            MailConfigHelper::load();
+        });
     }
 }
