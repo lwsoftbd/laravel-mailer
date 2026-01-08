94640352fb8ddeffffea6d684531ce1453a2dc17
lwsoftbd <dev@lwsoftbd.com>
2026-01-09 02:25:41 +0600

    Initial commit

--- src/Http/Controllers/SmtpController.php
+++ src/Http/Controllers/SmtpController.php
@@ -69,23 +69,24 @@ class SmtpController extends Controller
         return back()->with('success', 'Default SMTP Applied');
     }
 
-    // public function test(Request $request)
-    // {
-    //     $request->validate([
-    //         'email' => 'required|email'
-    //     ]);
+    public function testRaw(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email'
+        ]);
 
-    //     try {
-    //         Mail::raw("This is a test email from Laravel Mailer\n\nPowered by: LW Soft BD", function ($mail) use ($request) {
-    //         $mail->to($request->email)
-    //             ->subject('SMTP Test Email');
-    //         });
+        try {
+            Mail::raw("This is a test email from Laravel Mailer\n\nPowered by: LW Soft BD", function ($mail) use ($request) {
+            $mail->to($request->email)
+                ->subject('SMTP Test Email');
+            });
 
-    //         return back()->with('test_success', 'Test email sent successfully!');
-    //     } catch (\Exception $e) {
-    //         return back()->with('test_error', 'Failed to send test email: ' . $e->getMessage());
-    //     }
-    // }
+            return back()->with('test_success', 'Test email sent successfully!');
+        } catch (\Exception $e) {
+            return back()->with('test_error', 'Failed to send test email: ' . $e->getMessage());
+        }
+    }
+    
     public function test(Request $request)
     {
         $request->validate([
